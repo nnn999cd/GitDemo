76a8511e6d0f2b386c780d75252da092d71899f8
nnn999cd <takatomo.higa@ncd.co.jp>
2025-01-09 16:51:40 +0900

    mmmmm2

---
+++ src/introduction/RESTAPITest.java
@@ -0,0 +1,23 @@
+package introduction;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+	}
+	
+	@Test
+	public void  deleteTwitter() {
+		
+		System.out.println("deleteTwitter");
+	}
+
+}
